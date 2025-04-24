dfc863b45abe40df61f558b20a330b496e608fce
Brandon <stdntuvmath1@outlook.com>
2025-04-23 23:20:45 -0500

    Update Buy_UsingAVGSlope.cs

--- Buy_UsingAVGSlope.cs
+++ Buy_UsingAVGSlope.cs
@@ -14,8 +14,8 @@ namespace Stock_Trading_App
     internal static class Buy_UsingAVGSlope
     {
 
-        private static string API_KEY = "PKHNXT0M3ZBTEKBEH1LM";
-        private static string API_SECRET_KEY = "3EjNkXEQt7s4WceVGdmPslJ6oYo65uIzfHE09yPS";
+        private static string API_KEY = "removed";
+        private static string API_SECRET_KEY = "removed";
         private static string windowsUserName = System.Environment.UserName;//gives windows username
         private static string CurrentEquityFile = @"C:\Users\" + windowsUserName + @"\Documents\Paper Trader\CurrentEquity.txt";
         private static string CurrentCashMade = @"C:\Users\" + windowsUserName + @"\Documents\Paper Trader\CurrentCashMade.txt";
